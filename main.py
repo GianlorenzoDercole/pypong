e2a1e5fa853b7a48a42086ea362b86681e7264e8
GianlorenzoDercole <johndercole01@gmail.com>
2022-07-13 20:04:22 -0700

    render ball on screen

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ screen_width = 1200
 screen_height = 900
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
-
+# make the ball
+ball = pygame.Rect(585,435,30,30)
+white = (250,250,250)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+    screen.fill('grey')
+    # draw the ball
+    pygame.draw.ellipse(screen,white, ball)
 
 
     # update window
