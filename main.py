02c3f354f5743d7633c70a0f47c9c2a8a2248b28
GianlorenzoDercole <johndercole01@gmail.com>
2022-07-13 21:03:04 -0700

    ball moves and has collision with walls

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ pygame.display.set_caption('Pong')
 # make the ball
 ball = pygame.Rect(585,435,30,30)
 white = (250,250,250)
+ball_xdirection = 5
+ball_ydirection = 5
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    ball.x += ball_xdirection
+    ball.y += ball_ydirection
+    if ball.x > 1100 or ball.x < 100:
+        ball_xdirection = ball_xdirection * -1
+    if ball.y < 100 or ball.y > 800:
+        ball_ydirection = ball_ydirection * -1
+    # color for court
     screen.fill('grey')
     # draw the ball
     pygame.draw.ellipse(screen,white, ball)
