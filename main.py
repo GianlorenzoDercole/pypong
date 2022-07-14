48b98536eb3e5a6124423f99f9f0b31321b7fc3c
GianlorenzoDercole <johndercole01@gmail.com>
2022-07-14 09:46:19 -0700

    playerleft is now moving see line 25

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Pong')
 # make the ball
 ball = pygame.Rect(585,435,30,30)
+player_left = pygame.Rect(60,350,20,100)
+player_right = pygame.Rect(1140,350,20,100)
 white = (250,250,250)
+player_direction = 0
 ball_xdirection = 5
 ball_ydirection = 5
 while True:
@@ -19,8 +22,22 @@ while True:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                player_direction += 7
+            if event.key == pygame.K_w:
+                player_direction -= 7
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_s:
+                player_direction -= 7
+            if event.key == pygame.K_w:
+                player_direction += 7
+
+
+
     ball.x += ball_xdirection
     ball.y += ball_ydirection
+    player_left.y += player_direction
     if ball.x > 1100 or ball.x < 100:
         ball_xdirection = ball_xdirection * -1
     if ball.y < 100 or ball.y > 800:
@@ -29,7 +46,7 @@ while True:
     screen.fill('grey')
     # draw the ball
     pygame.draw.ellipse(screen,white, ball)
-
+    pygame.draw.rect(screen, white, player_left)
 
     # update window
     pygame.display.flip()
